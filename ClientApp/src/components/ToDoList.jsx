5aae2ba1a16ba523e15f0909b7c9c3102ba60b4e
mcm-ryan <mcm.ryan02@gmail.com>
2025-01-29 23:00:58 -0500

    Fixed breaking error. Need to resolve axios network error

--- ClientApp/src/components/ToDoList.jsx
+++ ClientApp/src/components/ToDoList.jsx
@@ -1,72 +1,65 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import axios from "axios";
-import { Divider, Radio, Table } from "antd";
+import { Table } from "antd";
 
 export class ToDoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ToDoList: [],
+      ToDoList: [],  
       loading: true,
     };
   }
 
-  populateTripsData() {
+  componentDidMount() {
+    this.populateToDoList();
+  }
+
+  populateToDoList() {
     axios
       .get("https://localhost:7036/api/ToDoList/GetToDoList")
       .then((result) => {
-        const response = result.data;
-        this.setState({ ToDoList: response, loading: false });
+        this.setState({ ToDoList: result.data, loading: false });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        this.setState({ loading: false });
       });
   }
 
   render() {
-    const [selectionType, setSelectionType] =
-      (useState < "checkbox") | ("radio" > "checkbox");
-
     const columns = [
       {
         title: "Title",
-        dataIndex: "name",
+        dataIndex: "title", 
+        key: "title",
         render: (text) => <a>{text}</a>,
       },
       {
         title: "Date Created",
-        dataIndex: "age",
+        dataIndex: "createdDate",  
+        key: "createdDate",
       },
     ];
 
-    // rowSelection object indicates the need for row selection
     const rowSelection = {
       onChange: (selectedRowKeys, selectedRows) => {
-        console.log(
-          `selectedRowKeys: ${selectedRowKeys}`,
-          "selectedRows: ",
-          selectedRows
-        );
+        console.log(`selectedRowKeys: ${selectedRowKeys}`, "selectedRows: ", selectedRows);
       },
       getCheckboxProps: (record) => ({
-        disabled: record.name === "Disabled User", // Column configuration not to be checked
-        name: record.name,
+        disabled: record.title === "Disabled User", 
+        name: record.title,
       }),
     };
 
     return (
-      <div>
-        <Radio.Group
-          onChange={(e) => setSelectionType(e.target.value)}
-          value={selectionType}
-        >
-          <Radio value="checkbox">Checkbox</Radio>
-          <Radio value="radio">radio</Radio>
-        </Radio.Group>
-        <Divider />
-        <Table
-          rowSelection={{ type: selectionType, ...rowSelection }}
-          columns={columns}
-          dataSource={this.state}
-        />
-      </div>
+      <Table
+        rowSelection={rowSelection}
+        columns={columns}
+        dataSource={this.state.ToDoList} 
+        loading={this.state.loading} 
+        rowKey="id" 
+      />
     );
   }
 }
